4e9cdf1bff7f5ee06de051c1f5eb03acc385f091
Yaroslav Khakhuda <hahyda14@gmail.com>
2022-11-17 10:30:34 +0200

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const instance = basicLightbox.create(`<img class="modal-img" src="">`);
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = galleryMarkup(galleryItems);
+galleryContainer.addEventListener('click', onGalleryClick);
+
+function galleryMarkup(items) {
+	return items
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
+				<a class="gallery__link" href="${original}">
+				<img
+					class="gallery__image"
+					src="${preview}"
+					data-source="${original}"
+					alt="${description}"
+				/>
+			</a>
+		</div>`;
+		})
+		.join('');
+}
+
+function onGalleryClick(e) {
+	e.preventDefault();
+	const currentImg = e.target;
+	if (!currentImg.classList.contains('gallery__image')) {
+		return;
+	}
+	const modalImg = instance.element().querySelector('.modal-img');
+	modalImg.src = currentImg.dataset.source;
+
+	window.addEventListener('keydown', closeModalESC);
+
+	instance.show();
+}
+
+function closeModalESC(e) {
+	if (e.code !== 'Escape') {
+		return;
+	}
+
+	instance.close();
+	window.removeEventListener('keydown', closeModalESC);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = galleryMarkup(galleryItems);
+
+const lightBoxOptions = {
+	captions: true,
+	captionsData: 'alt',
+	captionDelay: 250,
+};
+
+const lightbox = new SimpleLightbox('.gallery a', lightBoxOptions);
+function galleryMarkup(items) {
+	return items
+		.map(({ preview, original, description }) => {
+			return `<a class="gallery__item" href="${original}">
+  								<img class="gallery__image" src="${preview}" alt="${description}" />
+							</a>`;
+		})
+		.join('');
+}
